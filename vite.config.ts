dbb91954d09a18414bf604085314838a44902e2b
Rem <1941398048@qq.com>
2023-06-27 11:39:04 +0800

    init element-plus

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,25 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import eslint from 'vite-plugin-eslint'
 import { resolve } from 'path'
+
+import eslint from 'vite-plugin-eslint'
+
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), eslint()],
+  plugins: [
+    vue(),
+    eslint(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()]
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()]
+    })
+  ],
   resolve: {
     // ↓路径别名
     alias: {
