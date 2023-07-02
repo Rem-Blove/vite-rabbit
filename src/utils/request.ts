617b0e850808bd039f0f31ecaae9706232f8c21a
Rem <1941398048@qq.com>
2023-07-02 14:02:43 +0800

    <refactor>:完成验证token失效重新登录业务

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -2,7 +2,8 @@ import axios from 'axios'
 import { ElMessage } from 'element-plus'
 import nProgress from 'nprogress'
 import 'nprogress/nprogress.css'
-import { getToken } from './token'
+import { getToken, removeToken } from './token'
+import router from '@/routes'
 
 const requests = axios.create({
   baseURL: 'http://pcapi-xiaotuxian-front-devtest.itheima.net',
@@ -34,9 +35,14 @@ requests.interceptors.response.use(
     // 2xx 范围内的状态码都会触发该函数。
     // 对响应数据做点什么
     nProgress.done()
+
     return response.data
   },
   function (error) {
+    if (error.response.status === 401) {
+      removeToken()
+      router.push('/login') as any
+    }
     // 超出 2xx 范围的状态码都会触发该函数。
     // 对响应错误做点什么
     ElMessage({
